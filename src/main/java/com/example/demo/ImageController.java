c4807810e7b70db810545dfae06970a426a635b6
student <student@WIN-COS9ASH9F7S>
2023-06-14 10:51:25 +0900

    更新

--- src/main/java/com/example/demo/ImageController.java
+++ src/main/java/com/example/demo/ImageController.java
@@ -13,37 +13,37 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class ImageController {
 
-    @GetMapping("/")
-    public String show() {
-        return "image";
-    }
-    
-    /** 画像アップロード＆表示 */
-    @PostMapping("/upload")
-    public String uploadImage(
-            @RequestParam("file") MultipartFile file,
-            Model model) {
+	@GetMapping("/")
+	public String show() {
+		return "image";
+	}
 
-        try {
-            // ファイル名
-            String filename = file.getOriginalFilename();
-            // 保存先パス
-            String filePath = "static/image/" + filename;
-            // ファイルをバイナリデータとして取得
-            byte[] content = file.getBytes();
-            // 保存
-            Files.write(Paths.get(filePath), content);
-            
-            // アップロードした画像のURLを画面に渡す
-            String imageUrl = "/image/" + filename;
-            model.addAttribute("imageUrl", imageUrl);
-            
-        } catch (Exception e) {
-            // エラー時
-            e.printStackTrace();
-        }
+	/** 画像アップロード＆表示 */
+	@PostMapping("/upload")
+	public String uploadImage(
+			@RequestParam("file") MultipartFile file,
+			Model model) {
 
-        // image.htmlを描画
-        return "image";
-    }
+		try {
+			// ファイル名（別名をつけても良い）
+			String filename = file.getOriginalFilename();
+			// 保存先パス
+			String filePath = "static/image/" + filename;
+			// ファイルをバイナリデータとして取得
+			byte[] content = file.getBytes();
+			// 保存
+			Files.write(Paths.get(filePath), content);
+
+			// アップロードした画像のURLを画面に渡す
+			String imageUrl = "/image/" + filename;
+			model.addAttribute("imageUrl", imageUrl);
+
+		} catch (Exception e) {
+			// エラー時
+			e.printStackTrace();
+		}
+
+		// image.htmlを描画
+		return "image";
+	}
 }
